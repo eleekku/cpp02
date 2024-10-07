0e24d142eaadbdf27346ed4d018622cab649488a
eleekku <144835604+eleekku@users.noreply.github.com>
2024-10-07 17:21:00 +0300

    ex00 done

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	private:
+			int					_value;
+			static const int	_fixed;
+
+	public:
+			Fixed();
+			~Fixed();
+			Fixed(const Fixed &copy);
+			Fixed &operator=(const Fixed &copy);
+			int getRawBits(void) const;
+			void setRawBits(int const raw);		
+};
+
+#endif
